03bed8ba9dd9760a3a298518b831473c46aae50e
notTheLey <cm.ley.cm@gmail.com>
2025-03-11 09:53:16 +0100

    Update script.js

--- bobtonylogo/script.js
+++ bobtonylogo/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const applyChangesButton = document.getElementById('applyChanges');
     const resolutionInput = document.getElementById('resolution');
 
-    const svgFolder = 'svgs/';
+    const svgFolder = './svgs/';
 
     const originalSVGs = {};
     const editedSVGs = {};
